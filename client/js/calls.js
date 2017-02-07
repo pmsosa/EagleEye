6d4eda2d9c003bf7cb2d0ae47e62a8b57deb2216
pmsosa <sosavpm@gmail.com>
2017-02-07 02:57:58 +0000

    1. Front End will now draw and update recv/sent client graphs

--- client/js/calls.js
+++ client/js/calls.js
@@ -1,14 +1,45 @@
 /// CONSTANTS ///
-var getData_endpoint = "../getalldataset"
-var dataset = [] //This is the dataset object that will be acessed by all
-
+var getData_endpoint = "../getalldataset"	//This is the endpoint called to get information from flask
+var dataset = [] 	//This is the dataset object that will be acessed by getAllData
+var colors = {'red':"#FF0000", 'black':"#000000",'yellow':'#FFFF00','blue':'#0000FF','green':'#008000'} //List of colors for our graphs
+var chart_type = {'main':'usage'} //This dictionary specifies which type of graph we have set up initially for each chart.
 /// FUNCTIONS ///
+
+///TEMPORARY BULLSHIT
+chartdata = {
+	datasets: [{
+	        label: 'Host 1',
+	        data: [{ x: -10, y: 0 }, { x: 0, y: 10 }, { x: 10, y: 5 }],
+	        fill: false,
+	        borderColor: colors.red,
+	        pointRadius: 3
+	    	},
+	    	{
+	    	label: 'Host 2',
+	        data: [{ x: -5, y: 0 }, { x: 1, y: 10 }, { x: 6, y: 5 }],
+	        fill: false,
+	        borderColor: colors.blue,
+	        pointRadius: 1
+	    	}
+	    	]
+}
+
+options = {
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
+            }]
+        },
+        animation: false
+    }
 
 //Function call to get the the data from the ../getalldataset endpoint in flask
 function getAllData(){
 	$.get( getData_endpoint, function( data ) {
 	  console.log(data);
 
+	  //Check if we need to add any more clients to the website.
 	  if ((dataset.clients == undefined) || (data.clients.length > dataset.clients.length)){
 	  	n = 0
 	  	if (dataset.clients != undefined){ n = dataset.clients.length}
@@ -19,28 +50,27 @@ function getAllData(){
 	  	}
 
 	  }
-		// if ((data.clients.length > dataset.clients.length) || (dataset.clients == undefined)){
-		// 	//I'm assuming clients never re-organize on the list
-		// 	for (i = dataset.clients.length; i < data.clients.length){
-		// 		addClient(dataset.clients[i])
-		// 		console.log("Created: "+dataset.clients[i].mac)
-		// 	}
-		// }
 
-	  dataset = data 
+	  refreshClientGraphs(data)	//Refresh Client Graphs
+	  dataset = data 	//Update Dataset
+	  
+
 	  //alert( "Load was performed." );
 		  
 	});	
 }
 
 
-
+//Add a client to the front-end
 function addClient(client){
 	//new_client = document.getElementById('template_client_infobox').cloneNode(true)
 	new_client =$("#template_client_infobox")
 	new_client = new_client.html()
 	//Replacing IDs
-	new_client = new_client.replace("XXXX",client.mac)
+	while (new_client.search("XXXX") != -1){
+		new_client = new_client.replace("XXXX",client.mac)
+	}
+
 
 	//Replacing Placeholders
 	//new_client = new_client.replace("XXNAMEXX",client.name)
@@ -52,12 +82,124 @@ function addClient(client){
 	c = document.getElementById('client_list')
 	c.innerHTML += new_client
 
-	//Remember to Refreshing Graph after adding
-
+	chart_type[client.mac] = 0 //Initialize Chart Type
 }
 
-function refreshGraph(graph_id, datapoints,graphtype){
+function refreshClientGraphs(data){
+	clients = data.clients
 	//Refresh a graph with new datapoints
+  	for (i = 0; i < clients.length; i++){
+  		
+  		//Chart Type: 0 - Total Send/Recv usage
+  		if (chart_type[clients[i].mac] == 0){ 
+
+  			sentpoints = []
+  			recvpoints = []
+
+
+
+			for (j = 0; j < clients[i]["report"].length; j++){
+				report = clients[i]["report"]
+				//X-Axis
+				time = new Date(report[j][0]*1000)
+				//time = time.getHours()*100+ time.getMinutes()+(time.getSeconds()/100)
+				console.log(report)
+				//Y-Axis
+				sentpoints.push({x: time, y: report[j][1]["sent"]})
+				recvpoints.push({x: time, y: report[j][1]["recv"]})
+				//console.info(report)
+			}
+
+  			console.log(sentpoints)
+	  		chartdata = {
+				datasets: [
+							{label: 'Sent',
+					        data: sentpoints,
+					        fill: false,
+					        borderColor: colors.red,
+					        pointRadius: 3},
+
+					    	{label: 'Recieved',
+					        data: recvpoints,
+					        fill: false,
+					        borderColor: colors.blue,
+					        pointRadius: 1}
+				    	]
+			}
+
+			options = {
+				scales: {
+					xAxes:[{
+						type: 'time',
+                		position: 'bottom',
+						scaleLabel:{
+							display:true,
+							labelString:"Time"
+						},
+						time:{
+							displayFormats:{
+								millisecond: "h:mm:ss",
+								second: "h:mm:ss",
+								minute: "h:mm:ss",
+								hour: "h:mm:ss"
+							}
+						}
+					}],
+					yAxes:[{
+						scaleLabel:{
+							display:true,
+							labelString:"# of Packets"
+						}
+					}]
+				},
+				// scaleOverride: true,
+		  //       scales: {
+		  //           xAxes: [{
+		  //           	scaleSteps:1,
+				// 		scaleStepWidth: 10,
+		  //               type: 'time',
+		  //               position: 'bottom',
+		                // time:{
+		                // 	format: "h:mm:ss a",
+		                // 	unit: "second",
+		                // 	displayFormats:{
+		                // 		second: "h:mm:ss a",
+		                // 	}
+			                //min: (new Date((new Date()).getHours()-1)),
+			                //max: (new Date((new Date()).getHours()+1))
+		        //         }
+
+		        //     }]
+		        // },
+		        animation: false
+		    }	
+
+
+
+			new Chart(clients[i].mac+"_chart", {
+		    type: 'line',
+		    data: chartdata,
+		    options: options
+			});
+		}
+
+		//Chart Type: 1 - UDP/TCP
+		else if (chart_type[clients[i].mac] == 1){
+
+		}
+		
+		//Chart Type: 3 - Portwise Chart
+		else if (chart_type[clients[i].mac] == 2){
+
+		}
+
+		//Chart Type: 4 - Portwise Chart
+		else{
+			//Some Default.
+		}
+
+
+	}
 }
 
 
