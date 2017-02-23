845d43c359fc1c13fd655fa9ef5ea5ac957a5d6b
pmsosa <sosavpm@gmail.com>
2017-02-23 13:24:52 -0800

    Fixed timing issue when reading pcap files. Note you can only feed the fake monitor PCAP files and not PCAPNG. It will mess it up.

--- packetCapture.py
+++ packetCapture.py
@@ -9,7 +9,7 @@ endpoint = "setPacketCaptr"
 iface = "tap0"
 timeout = 120 # For unlimited time set = None
 
-timestep = 1 # 5 Seconds (Equivalent to the granularity of the graphs)
+timestep = 5 # 5 Seconds (Equivalent to the granularity of the graphs)
 clients = []
 clients_names = []
 debug = False
@@ -28,10 +28,10 @@ class Client:
     #last_timestamp = [time.time(),time.ctime()] # [Millisecs,Timestamp]
 
     def __init__(self,mac,ip="",name="",os="",debug=False):
-        if (Client.timesteps == []): Client.timesteps = [time.time()];
+        #if (Client.timesteps == []): Client.timesteps = [time.time()];
         self.mac = mac   # MAC
         self.ip = ip     # IP
-        self.name = name   # Gotten by Nmap Sweep
+        self.name = name # Gotten by Nmap Sweep
         self.os = os     # Mac, Linux, Windows, Other, N/A
         self.report = [[Client.timesteps[-1],copy.deepcopy(self.template)]] # Probably a list of dictionaries [{'timestamp':'9:00:00','https':'20%','udp':'80%'},...]
         self.leak = [];
@@ -39,7 +39,7 @@ class Client:
 
     def record_packet(self,packet):
         #print ".",
-        t = packet.payload.time #t = time.time()
+        t = packet.time #t = packet.payload.time #t = time.time()
         
         if (self.debug):
             print "-----------------------"
@@ -52,7 +52,7 @@ class Client:
             element = self.report[len(self.report)-1][1] #This is a shallow copy        
             
         else:
-            Client.timesteps += [time.time()]
+            Client.timesteps += [t] #[time.time()]
             if self.debug: print "<< Out of Window!"
             #Packet is outside of window size   
             element = copy.deepcopy(self.template)
@@ -95,14 +95,15 @@ class Client:
     def check_leaks(self,packet):
         try:
             if (packet[Raw].load[:4] == "POST"):
-                print "Post found"
+                
                 raw = packet[Raw].load.lower()
-                r1 = raw[raw.index("host:"):]
-                host = r1[:r1.index("\r\n")].replace("host:","")
-                content = raw[raw.index("content-length:"):]
-                tleak = {"host":host,"content":content}
-
-                self.leak += [tleak];
+                if ("pass" in raw):
+                    print "Leak Found!"
+                    r1 = raw[raw.index("host:"):]
+                    host = r1[:r1.index("\r\n")].replace("host:","")
+                    content = raw[raw.index("content-length:"):]
+                    tleak = {"host":host,"content":content}
+                    self.leak += [tleak];
         except:
             pass;
         #check if they have something called password or login
@@ -129,9 +130,10 @@ def fake_monitor(n,capture_file):
 def rec_packet(packet):
     global clients_names,clients,debug
     try:
-        if Client.timesteps == []: Client.timesteps += [packet.payload.time]
+        if Client.timesteps == []: Client.timesteps += [packet.time] #[packet.payload.time]
 
-        t = packet.payload.time #t = time.time()        
+        t = packet.time #t = packet.payload.time #t = time.time()        
+        #print t, str(Client.timesteps[-1]);
         ## --- Should we send an Update to Flask Server--- ##
         if (t > Client.timesteps[-1] + timestep):
             upload_data()
@@ -141,6 +143,7 @@ def rec_packet(packet):
         if packet.src not in clients_names:
             #Add client to our dataset if we haven't seen 'em before.           
             clients_names += [packet.src]
+            ##
             clients += [Client(packet.src,packet.getlayer(IP).src,packet.src,"Unknown")]
         else:
             #Dangreous (but I don't want to do linear search each time)
@@ -190,7 +193,7 @@ def upload_data():
 if __name__ == "__main__":
     if "fake" in sys.argv:
         #Fake Monitor - 
-        capture = "capture_examples/http_leak.pcapng"
+        capture = "capture_examples/kaleo.pcap" #WARNING: PCAP FILES ONLY. (Timing will be off if you are using pcapng files.)
         fake_monitor(600,capture)
     else:
         monitor(timeout,iface)
